3a8b6374e5f0a7d4c66cf1586d3a6cb509f290b5
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-04-02 16:39:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 // write your code below!
 
 var string = "Happy holidays!"
+
 function happyHolidays() {
   return string
+}
+
+function happyHolidaysTo(name) {
+  
+  return `${string}, ${name}`
+  console.log('')
 }
